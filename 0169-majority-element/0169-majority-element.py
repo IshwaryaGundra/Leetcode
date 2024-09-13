710133bfb826ccef49dac33cf888e64db796df21
Ishwarya <97424580+IshwaryaGundra@users.noreply.github.com>
2024-09-13 16:12:58 +0530

    Time: 164 ms (84.11%), Space: 18.1 MB (21.33%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d=Counter(nums)
+        for i in (d):
+            print(d[i])
+            if d[i]>len(nums)/2:
+                return i
+        return -1
+
+        
